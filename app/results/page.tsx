da234e0f210604c7d0165999beccf453cba4e073
mattdevops <mattstermh@yahoo.com>
2026-03-04 12:26:04 -0500

    updated theme

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -164,9 +164,9 @@ function ResultsContent() {
               <Image
                 src="/logo.png"
                 alt="Strategy Reality Check"
-                width={180}
-                height={40}
-                className="h-8 w-auto"
+                width={240}
+                height={60}
+                className="h-12 md:h-14 w-auto"
               />
             </Link>
           </div>
